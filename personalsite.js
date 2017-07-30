e7dffa205738cb54b81357f586647013597f894b
chrisfio <chris.fiorino@gmail.com>
2017-07-30 19:24:02 -0400

    made css tweaks

--- personalsite.js
+++ personalsite.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
 	$(".section").hide();
+
+/* Toggle Buttons*/
+/* About Me Section*/
+	$("#aboutme").click(function(){
+		$("#aboutmecontent").toggle();
+	});
+
+/* Experience Section*/
+	$("#experience").click(function(){
+		$("#experiencecontent").toggle();
+	});
+
+/* Education Section*/
+	$("#education").click(function(){
+		$("#educationcontent").toggle();
+	});
+
+/* Skills and Course Work Section*/
+	$("#skills").click(function(){
+		$("#skillscontent").toggle();
+	});
+
+
+
 });
