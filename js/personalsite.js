ea34f7df90ba7fac12657db41046eb23c6911f2f
chrisfio <chris.fiorino@gmail.com>
2017-10-06 13:12:52 -0700

    Reduced sizing of fonts, adjusted standard laptop view, many fonts, icons and padding were far too large. Also adjusted sizing for tablet

--- js/personalsite.js
+++ js/personalsite.js
@@ -5,46 +5,58 @@ $(document).ready(function(){
 	    });
 	});
 
-	$('#circle-aboutme-hover').mouseenter(function(){
+	$('#circle-aboutme-hover').hover(function(){
 		$('#circle-text-aboutme').show();
 		$('#circle-aboutme').css('background','#6fffe9');
-	}).mouseleave(function(){
+	}, function(){
 		$('#circle-text-aboutme').hide();
 		$('#circle-aboutme').css('background','none');
 	});
 
-	$('#circle-experience-hover').mouseenter(function(){
+	$('#circle-experience-hover').hover(function(){
 		$('#circle-text-experience').show();
 		$('#circle-experience').css('background','#6fffe9');
-	}).mouseleave(function(){
+	}, function(){
 		$('#circle-text-experience').hide();
 		$('#circle-experience').css('background','none');
 	});
 
-	$('#circle-projects-hover').mouseenter(function(){
+	$('#circle-projects-hover').hover(function(){
 		$('#circle-text-projects').show();
 		$('#circle-projects').css('background','#6fffe9');
-	}).mouseleave(function(){
+	}, function(){
 		$('#circle-text-projects').hide();
 		$('#circle-projects').css('background','none');
 	});
 
-	$('#circle-skills-hover').mouseenter(function(){
+	$('#circle-skills-hover').hover(function(){
 		$('#circle-text-skills').show();
 		$('#circle-skills').css('background','#6fffe9');
-	}).mouseleave(function(){
+	}, function(){
 		$('#circle-text-skills').hide();
 		$('#circle-skills').css('background','none');
 	});
 
-	$('#circle-contactme-hover').mouseenter(function(){
+	$('#circle-contactme-hover').hover(function(){
 		$('#circle-text-contactme').show();
 		$('#circle-contactme').css('background','#6fffe9');
-	}).mouseleave(function(){
+	}, function(){
 		$('#circle-text-contactme').hide();
 		$('#circle-contactme').css('background','none');
 	});
 
+	$('#intro-arrow').hover(function(){
+		$('#arrow-see-more').show();
+	}, function(){
+		$('#arrow-see-more').hide();
+	});
+
+	$('a').click(function(){
+	    $('html, body').animate({
+	        scrollTop: $( $(this).attr('href') ).offset().top
+	    }, 500);
+	    return false;
+	});	
 
 
 })
